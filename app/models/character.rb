3290e06b31b6e35371c0e8da3609a49930ff6a75
Arthur Del Esposte <arthurmde@gmail.com>
2015-04-07 22:03:05 -0300

    Add characters tests

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,20 +6,6 @@ class Character < ActiveRecord::Base
   has_many :participations
   has_many :comics, through: :participations
 
-  def self.find_all
-    characters = []
-    results = Marvel.all_characters
-    results.each do |result|
-      puts "="*100, results, "="*100
-      thumbnail = result['thumbnail'] ? result['thumbnail']['path'] : ""
-      characters << Character.new(character_id: result['id'],
-                       name: result['name'],
-                       thumbnail: thumbnail,
-                       description: result['description'])
-    end
-    characters
-  end
-
   class << self
     # finds a character by its name. This method is a shortcut to
     #
--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,7 +1,41 @@
 require_relative "../test_helper"
 
 class CharacterTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "should not save a character without an Marvel ID" do
+    character = Character.new
+    assert_not character.save
+  end
+
+  test "should not save a character with an existing Marvel ID" do
+    character = Character.new(:character_id => 1009227)
+    assert_not character.save
+  end
+
+  test "should save a character with a properly Marvel ID" do
+    character = Character.new(:name => "Bishop", :character_id => 1009182)
+    assert character.save
+  end
+
+  test "should find existing character by his name" do
+    character = Character["Black Cat"]
+    assert character
+
+    character = Character["Carnage"]
+    assert character
+  end
+
+  test "should not find non existing character by name" do
+    character = Character["Cable"]
+    assert_nil character
+  end
+
+  test "should differentiate character's type" do
+    villain = Character["Carnage"]
+    hero = Character["Spider-Man"]
+
+    assert villain.class != hero.class
+
+    assert_equal "Hero", hero.type
+    assert_equal "Villain", villain.type
   end
 end
